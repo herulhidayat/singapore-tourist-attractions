2da5b1921d626cba3aed1ebbfadd9ab880106f89
herulhidayat <47564314+herulhidayat@users.noreply.github.com>
2023-01-23 16:10:35 +0700

    added leaflet

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { SidebarService } from '../../services/sidebar.service'
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit,AfterViewInit {
   isActive: boolean = false;
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map').setView([1.290270, 103.851959], 15);
+
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 17,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+  
   constructor(public sidebarService: SidebarService) { }
 
   ngOnInit(): void {
     this.sidebarService.isActive.subscribe((isActive) => {
       this.isActive = isActive;
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 }
